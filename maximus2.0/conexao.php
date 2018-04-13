9c1ccf3c04cc61a4b011ebc303d7abf88e5ce10a
OSPrevencao <scn.ads.ope.manha@gmail.com>
2018-04-13 11:47:17 -0300

    Update conexao.php

--- maximus2.0/conexao.php
+++ maximus2.0/conexao.php
@@ -1,20 +1,20 @@
 <?php
     //conexao local
 	
-	$servidor = "127.0.0.1";
-	$usuario = "root";
-	$senha = "";
-	$dbname = "osprevencao";
+	//$servidor = "127.0.0.1";
+	//$usuario = "root";
+	//$senha = "";
+	//$dbname = "osprevencao";
 	
 	//Criar a conexão
-	$conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
+	//$conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
 
     //conexao no servidor
-    // $servidor = "mysql472.umbler.com";
-    // $usuario = "osprevencao";
-    // $senha = "sistemadovo";
-    // $dbname = "osprevencao";
+     $servidor = "mysql472.umbler.com";
+     $usuario = "osprevencao";
+     $senha = "sistemadovo";
+     $dbname = "osprevencao";
     
     // //Criar a conexão
-    // $conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
+     $conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
 
