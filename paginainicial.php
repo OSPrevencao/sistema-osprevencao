45796bf3b66d8214f61931e6dbad36ee15f1e51c
OSPrevencaoo <scn.ads.ope.manha@gmail.com>
2018-06-08 17:38:38 -0300

    ajustes

--- paginainicial.php
+++ paginainicial.php
@@ -42,14 +42,14 @@
 				</div>
 				<div class="modal-body">
 					<div class="form-group">
-						<div class="form-row">
+						<!-- <div class="form-row">
 							<label for="tipo_agenda">Escolha uma agenda para inserir o Evento:</label>
 							<select class="form-control" name="tipo_agenda" id="tipo_agenda">
 								<option value="obra">Agenda de Obra</option>
 								<option value="visita">Agenda de Visita</option>
 							</select>
-						</div>
-						<br>
+						</div> -->
+						<!-- <br> -->
 						<div class="form-row">
 							<label for="nome-evento">Nome do Evento:</label>
 							<input type="text" name = "nome-evento" id = "nome-evento" class="form-control" placeholder="Nome do evento">
