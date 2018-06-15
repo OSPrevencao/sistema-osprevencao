afffc4df74e813f4a487add053c7187e28ca664f
OSPrevencaoo <scn.ads.ope.manha@gmail.com>
2018-06-15 16:44:48 -0300

    ajustes

--- conexao_example.php
+++ conexao_example.php
@@ -1,9 +1,10 @@
 <?php
-    $servidor = "127.0.0.1";
-    $usuario = "root";
-    $senha = "";
-    $dbname = "osprevencao2";
+    $servidor = "mysql785.umbler.com";
+     $usuario = "osprevencao2";
+     $senha = "sistemadovo1";
+     $dbname = "osprevencao2";
     
-    // Criar a conexão
-   $conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
+    //Criar a conexão
+     $conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
+
      mysqli_set_charset($conn, 'utf8');
